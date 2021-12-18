a38acfee402aebb9bcef9d5b1f25f458920ec118
deide <deide@DESKTOP-LERAY>
2021-12-18 17:20:26 +0300

    LearnSchool WPF

---
+++ LearnSchool/LearnSchool/AuthorizationPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnSchool
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Transition.MainFrame.Navigate(new ServicesPage());
+            MessageBox.Show("Вход выполнен успешно");
+        }
+    }
+}
---
+++ LearnSchool/LearnSchool/ServicesPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnSchool
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicesPage.xaml
+    /// </summary>
+    public partial class ServicesPage : Page
+    {
+        public ServicesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddService_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("В разработке");
+        }
+
+        private void DeleteService_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("В разработке");
+        }
+
+        private void BtnChangeService_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("В разработке");
+        }
+    }
+}
